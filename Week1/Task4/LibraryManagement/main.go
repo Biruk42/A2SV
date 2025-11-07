b9f62fba78e5deef5335e155ecd61694b5fcd3f7
Biruk <kassayebiruk0@gmail.com>
2025-11-07 20:16:03 +0300

    Refactor main function to improve book addition readability

--- Week1/Task4/LibraryManagement/main.go
+++ Week1/Task4/LibraryManagement/main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"library_management/controllers"
+	"library_management/models"
 	"library_management/services"
 )
 
 func main() {
 	library := services.NewLibrary()
+
+	library.AddBook(models.Book{ID: 1, Title: "Atomic Habit", Author: "Biruk"})
+	library.AddBook(models.Book{ID: 2, Title: "Library", Author: "Biruk"})
+
+
+
 	controller := controllers.NewLibraryController(library)
 	controller.Run()
 }
