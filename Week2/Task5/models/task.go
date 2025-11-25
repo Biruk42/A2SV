e5bc3de6d231f68aa91db78640bdbd863b8c8138
Biruk <kassayebiruk0@gmail.com>
2025-11-25 20:42:24 +0300

    Initialize task manager module and define Task models

---
+++ Week2/Task5/models/task.go
@@ -0,0 +1,19 @@
+package models
+
+
+import "time"
+
+type Task struct {
+ID string `json:"id"`
+Title string `json:"title" binding:"required"`
+Description string `json:"description,omitempty"`
+DueDate time.Time `json:"due_date"` 
+Status string `json:"status"` 
+}
+
+type TaskInput struct {
+Title string `json:"title" binding:"required"`
+Description string `json:"description"`
+DueDate string `json:"due_date"` 
+Status string `json:"status"` 
+}
