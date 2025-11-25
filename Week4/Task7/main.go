acea0fcb93c8003e02ac3ced903e4d51df13b2e4
Biruk <kassayebiruk0@gmail.com>
2025-11-25 21:46:41 +0300

    fix: update MongoDB connection string from localhost to 127.0.0.1

--- Week4/Task7/main.go
+++ Week4/Task7/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	// Initialize MongoDB
-	err := data.InitMongoDB("mongodb://localhost:27017", "task_manager")
+	err := data.InitMongoDB("mongodb://127.0.0.1:27017", "task_manager")
 	if err != nil {
 		log.Fatal("Failed to connect to MongoDB:", err)
 	}
